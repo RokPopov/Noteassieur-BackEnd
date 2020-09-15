629325193b735b4b1daf3c0e946bfb868a31c723
rokpopov <rokpopov90@gmail.com>
2020-09-15 11:03:51 +0200

    Create lecture model

---
+++ migrations/20200915085913-create-lecture.js
@@ -0,0 +1,35 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("lectures", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      time: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        unique: true,
+      },
+      url: {
+        type: Sequelize.TEXT,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("lectures");
+  },
+};
---
+++ models/lecture.js
@@ -0,0 +1,34 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class lecture extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  lecture.init(
+    {
+      title: {
+        type: DataTypes.STRING,
+      },
+      time: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        unique: true,
+      },
+      url: {
+        type: DataTypes.TEXT,
+      },
+    },
+    {
+      sequelize,
+      modelName: "lecture",
+    }
+  );
+  return lecture;
+};
